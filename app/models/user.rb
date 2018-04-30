8c7572ad8394c9c2554e501527143f1005b62433
Steffany Brown <steffannbrown@gmail.com>
2018-04-30 13:51:19 -0700

    adding login functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   has_many :ranked_works, through: :votes, source: :work
 
   validates :username, uniqueness: true, presence: true
+
+  def self.build_from_github(auth_hash)
+    return User.new(
+      provider: auth_hash[:provider], uid: auth_hash[:uid], username: auth_hash[:info][:nickname]
+    )
+  end
 end
