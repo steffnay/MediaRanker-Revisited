bc5ed974ccc78b3068d60df2e6ec4f73161daccc
Steffany Brown <steffannbrown@gmail.com>
2018-04-30 22:10:41 -0700

    added user controller tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,4 +2,32 @@ require 'test_helper'
 
 describe UsersController do
 
+  describe "logged in users" do
+    before do
+      user = users(:grace)
+      login(user)
+    end
+
+    describe "index" do
+      it "succeeds when there are users" do
+
+        get users_path
+        must_respond_with :success
+      end
+    end
+
+    describe "show" do
+      it "succeeds for an extant user ID" do
+        user = users(:ada)
+
+        get user_path(user.id)
+        must_respond_with :success
+      end
+
+      it "renders 404 not_found for a bogus user ID" do
+        get user_path("abc")
+        must_respond_with :not_found
+      end
+    end
+  end
 end
